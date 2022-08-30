744a0636cbfe1166fe6fd49c1409d38b62d867c7
K.M Ahnaf Zamil <ahnaf@ahnafzamil.com>
2022-08-30 17:35:56 +0600

    Fixed Windows data.db

--- local-api/src/app.py
+++ local-api/src/app.py
@@ -1,4 +1,5 @@
 import os
+import re
 from flask import Flask
 from flask_cors import CORS
 from .lib.ext import db
@@ -6,13 +7,19 @@ from .lib.ext import db
 
 def make_app():
     from dotenv import load_dotenv
-
     load_dotenv()
+
+    db_path_raw = os.path.join(os.getcwd(), 'data.db')
+
+    if os.name == "nt":
+        db_path_raw = db_path_raw.replace("\\", "\\\\")
+    else:
+        db_path_raw = "/" + db_path_raw # Linux absolute path
 
     app = Flask(__name__)
     app.config[
         "SQLALCHEMY_DATABASE_URI"
-    ] = f"sqlite:////{os.path.join(os.getcwd(), 'data.db')}"
+    ] = f"sqlite:///{db_path_raw}"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
